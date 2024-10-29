cefeea6f8fb412887bc72510327521f656213ec2
Emmanuelgessessew <163059051+Emmanuelgessessew@users.noreply.github.com>
2024-10-29 14:25:51 +0000

    editing

--- Sprint-1/exercises/random.js
+++ Sprint-1/exercises/random.js
@@ -11,6 +11,17 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // answer
 //here the num is any integer between the value of 1 to 100, to breakdown the code
 //  the function Math.random() calls any number between 0 to 0.9999. lets say 0.5575
+
+
 // and this Math.random() * (maximum - minimum + 1) calculates the random number we got by (maximum - minimum + 1), which can be (100-1+1) which will give us the result of 100.
+
+
 //  (0.5575 *100) = 55.75
+ 
+
 // the function Math.floor will round the result of the previous step down to the nearest whole number, giving a random integer between 0 and 99 which in this will be 55.
+
+
+// finally add the minimum to the result
+
+console.log(num);
